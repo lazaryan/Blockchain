769ca48c382be36a13a5b838e5df005b0e66df8e
lazaryan <lazaryan99@gmail.com>
2019-02-23 22:53:51 +0300

    add function clear directory

--- directorys.py
+++ directorys.py
@@ -26,3 +26,19 @@ class Directory (object):
         :return: Ничего не возвращает
         """
         shutil.rmtree(path, ignore_errors=False)
+
+    @staticmethod
+    def _clear_directory(path):
+        """Метод очистки директории
+
+        Метод очищает все содержимое директории
+        Включая все вложенне файлы и подкаталоги
+
+        :param path: Путь к директории
+        :return: Ничего не возвращает
+        """
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                os.unlink(os.path.join(root, file))
+            for directory in dirs:
+                shutil.rmtree(os.path.join(root, directory))
